40483694919c99c0f2adb524e7eb87257dbbe329
julian <julian.urrutia@areamovil.com.co>
2021-02-27 14:25:12 -0500

    se modifica datos mocks

--- TecnicalTest/Repository/Breeds/Mocks/BreedRepositoryMocks.swift
+++ TecnicalTest/Repository/Breeds/Mocks/BreedRepositoryMocks.swift
@@ -10,9 +10,9 @@ import Foundation
 class BreedRepositoryMocks: BreedRemoteRepositoryProtocol {
     
     let breeds: [Breed] = [
-        Breed(arlternativeName: "", id: "AngXmatadora", name: "Angora", description: "La mejor", origin: "Mi casita", temperament: "Suave", wikipediaURL: "https://google.com"),
-        Breed(arlternativeName: "", id: "lamp1n10", name: "Lampiño", description: "El peor", origin: "Egipto maybe", temperament: "Se ve bravo", wikipediaURL: "https://google.com"),
-        Breed(arlternativeName: "", id: "cr10ll1t0", name: "Criollito", description: "Si compras animales, te denuncio", origin: "Abandono T_T", temperament: "Hermosos, ADOPTE!", wikipediaURL: "https://google.com"),
+        Breed(arlternativeName: "", id: "prueba", name: "prueba name", description: "prueba desc", origin: "prueba casita", temperament: "prueba2", wikipediaURL: "https://google.com"),
+        Breed(arlternativeName: "", id: "prueba", name: "prueba", description: "prueba", origin: "prueba", temperament: "prueba3", wikipediaURL: ""),
+        Breed(arlternativeName: "", id: "prueba4", name: "prueba", description: "prueba", origin: "prueba", temperament: "prueba", wikipediaURL: "https://google.com"),
     ]
     
     func getBreedList(completion: @escaping (Result<[Breed], NetworkError>) -> Void) {
